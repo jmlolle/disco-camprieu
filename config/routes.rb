cdee45f192ab16b08a7c88c847792209ed944226
Votre nom ou pseudo <maxime.malafosse@gmail.com>
2018-07-09 23:20:27 +0200

    ouep

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  get "/student", to: "visitors#student", as: "student"
-  get "/marseille", to: "visitors#marseille", as: "marseille"
-end
+  end
